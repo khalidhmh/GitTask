2f7f8d830b192d2a1778c9a1ded8ffdef4887490
farediaa2005 <faresdiaa2005@gmail.com>
2025-07-30 00:03:32 +0300

    Update HelloITI.java

--- HelloITI.java
+++ HelloITI.java
@@ -1,4 +1,5 @@
 public class HelloITI {
+     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
         int num1, num2;
         System.out.print("Enter frist number: ");
@@ -6,4 +7,5 @@ public class HelloITI {
         System.out.print("Enter sceant number: ");
         num2 =in.nextInt();
         System.out.println(num1+" * "+num2+" = " +num1*num2);
+     }
 }
