ab3ddceb0819a1485b4567354e1eb24a79bd5951
Ahmed-Rajab1 <ahmedrajabjaber@gmail.com>
2025-07-29 23:59:33 +0300

    editing hello message from ahmed

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+class helloWorld {
+
+    public static void helloWorldprintfunc() {
+        System.out.println("Hello, World! from Ahmeds side");
+    }
+}
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        helloWorld.helloWorldprintfunc();
+    }
+}
