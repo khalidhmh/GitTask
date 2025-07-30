7dcdfc130c2add255e406e56cd41a57bd4ecf4a7
farediaa2005 <farediaa2005@gmail.com>
2025-07-30 23:01:46 +0300

    from fares111

--- HelloITI.java
+++ HelloITI.java
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
 public class HelloITI {
      public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
         int num1, num2;
+        ///fares
         System.out.print("Enter frist number: ");
         num1 =in.nextInt();
         System.out.print("Enter sceant number: ");
         num2 =in.nextInt();
         System.out.println(num1+" * "+num2+" = " +num1*num2);
      }
-=======
-public class HelloITI{
-  public static void main (String [] args){
-    
-  }
->>>>>>> 456d01808c5b429d332601600c4e784e71c7b706
+}
+
 }
